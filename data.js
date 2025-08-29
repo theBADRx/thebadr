2c298bd33f8c972cccac1ebf4c06f3d8c54e398c
theBADRx <qq55q@hotmail.com>
2025-08-29 14:16:50 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+
+const DATA = {
+  Riyadh: {
+    mapQuery: "Riyadh%20tourist%20attractions",
+    defaults: [
+      ["Diriyah", "Najd Village (Lunch)", "Camel Step Coffee"],
+      ["National Museum", "Al Orjouan (Dinner)", "Kingdom Tower"],
+      ["Boulevard City", "Lusin Restaurant", "Brew92"],
+      ["Shopping Tour", "Local Coffee Hubs"]
+    ],
+    alternatives: ["Souq Al-Zal", "Wadi Hanifah", "The Globe", "Riyadh Zoo"]
+  },
+  Jeddah: {
+    mapQuery: "Jeddah%20tourist%20attractions",
+    defaults: [
+      ["Corniche Walk", "Al Baik", "Medd Café"],
+      ["Historic Al-Balad", "Twina Seafood", "Red Sea Mall"],
+      ["Boat Trip", "Seafood Night", "Cup & Couch"],
+      ["Jeddah Fountain", "Myazú"],
+      ["Relax", "Coffee Crawl"]
+    ],
+    alternatives: ["Aquarium", "Obhur Beach", "Waterfront Park"]
+  },
+  AlUla: {
+    mapQuery: "AlUla%20Saudi%20Arabia%20attractions",
+    defaults: [
+      ["Hegra", "Suhail"],
+      ["Elephant Rock", "Pink Camel"],
+      ["Stargazing", "Somewhere Café"]
+    ],
+    alternatives: ["Old Town", "Hot Air Balloon", "Harrat Viewpoint"]
+  }
+};
